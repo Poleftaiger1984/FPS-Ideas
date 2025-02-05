4c21cb167ff713ebc3de95e33ee4d8569d3f275c
Poleftaiger1984 <13319178+Poleftaiger1984@users.noreply.github.com>
2025-02-06 00:26:58 +0200

    Revert "Initial Commit - Implemented basic character movement including Crouching, Sprinting and Double Jumping. Added a FPS Arms Mesh for the player"
    This reverts commit 3d8bdfd0bbc887f3af88864932c7a2eb3bf6d7a1.

--- Source/FPIdeas/Public/Characters/CharacterStates.h
+++
@@ -1,9 +0,0 @@
-#pragma once
-
-UENUM(BlueprintType)
-enum class ECharacterState : uint8
-{
-	ECS_Idle UMETA(DisplayName = "Idle"),
-	ECS_Crouching UMETA(DisplayName = "Crouching"),
-	ECS_Sprinting UMETA(DisplayName = "Running")
-};
